d517141a2d93003d35ada8aae5ce01c052a03d30
MicaelaGuevara <mika.m.guevara.19.20@gmail.com>
2025-08-20 22:36:17 +0000

    .

--- JavaScript/Ciclos/Clase_1/Ciclos_1-2-3.py
+++ JavaScript/Ciclos/Clase_1/Ciclos_1-2-3.py
@@ -13,13 +13,3 @@ do{
     conteo++;
 }while(conteo < 3);
 console.log("Fin del ciclo do while")
-
-//Ciclo For
-
-let conteo = 0;
-do{
-    console.log(conteo);
-    conteo++;
-}while(conteo < 3);
-console.log("Fin del ciclo while")
-
