fd6cedf07ba6a371d84bb06ceebeab5d7d2f062e
Samira <bazsamira79@gmail.com>
2025-08-20 21:07:52 -0300

    "Organicé mejor los ejercicios de ciclos de la clase 1"

---
+++ JavaScript/Ciclos/Ejercicios Ciclos/ejercicioCiclosPSeInt/ejercicio1.js
@@ -0,0 +1,6 @@
+let i = 1; // Definir i como entero e inicializar en 1
+
+while (i <= 10) { // Mientras i sea menor o igual a 10
+  console.log(i); // Escribir i en consola (una línea por número)
+  i = i + 1; // Incrementar i en 1 para evitar bucle infinito
+}
---
+++ JavaScript/Ciclos/Ejercicios Ciclos/ejercicioCiclosPSeInt/ejercicio2.js
@@ -0,0 +1,9 @@
+let i = 1;
+let fila = ""; // Creamos una cadena vacía para ir acumulando los números
+
+while (i <= 10) {
+  fila += i + " "; // Concatenamos el número y un espacio
+  i = i + 1;
+}
+
+console.log(fila); // Imprimimos toda la fila en una sola línea
