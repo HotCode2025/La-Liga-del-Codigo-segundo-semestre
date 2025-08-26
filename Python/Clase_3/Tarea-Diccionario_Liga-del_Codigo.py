2dabc5f585a1cfd2d41091fd8b0ac3b956fcd418
Maximiliano <maxiarielmorales712@gmail.com>
2025-08-26 01:00:16 +0000

    Agregué dos lineas de código, con dos jugadores más. E hice un correccion en la linea 16

--- Python/Clase_3/Tarea-Diccionario_Liga-del_Codigo.py
+++ Python/Clase_3/Tarea-Diccionario_Liga-del_Codigo.py
@@ -5,11 +5,13 @@ seleccionArgentina = {
     "22": {"Nombre": "Lautaro Martinez", "Edad": 27, "Altura": 1.74, "Precio": "95 Millones", "Posicion": "Delantero"},
     "3": {"Nombre": "Lisandro Martinez", "Edad": 26, "Altura": 1.75, "Precio": "50 Millones", "Posicion": "Defensa Central"},
     "20": {"Nombre": "Alexis Mac Allister", "Edad": 25, "Altura": 1.76, "Precio": "100 Millones", "Posicion": "Mediocampista"},
-    "9": {"Nombre": "Julian Alvarez", "Edad": 24, "Altura": 1.70, "Precio": "100 Millones", "Posicion": "Delantero"}
+    "9": {"Nombre": "Julian Alvarez", "Edad": 24, "Altura": 1.70, "Precio": "100 Millones", "Posicion": "Delantero"},
+    "8": {"Nombre": "Marcos Javier Acuña", "Edad": 33, "Altura": 1.72, "Precio": "2 Millones", "Posicion": "Lateral izquierdo"},
+    "5": {"Nombre": "Leandro Paredes", "Edad": 31, "Altura": 1.80, "Precio": "5 Millones", "Posicion": "Pivote"}
 }
 
 for llave, valor in seleccionArgentina.items():
     print(llave, valor)
 
-print("Tenemos cargados en el diccionario la cantidad de jugadores:", end=" ")
+print("Tenemos cargados en el diccionario la cantidad de jugadores: ", end=" ")
 print(len(seleccionArgentina))
