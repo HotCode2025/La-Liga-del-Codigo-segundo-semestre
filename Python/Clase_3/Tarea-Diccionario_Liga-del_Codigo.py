c9ceed7d190322364275c2e916b2f1a00ece0a00
Ian Ditlevsen <ian.ditlevsen.2011@gmail.com>
2025-08-25 21:21:15 -0300

    Ejercicio-Diccionario-Clase_3

---
+++ Python/Clase_3/Tarea-Diccionario_Liga-del_Codigo.py
@@ -0,0 +1,15 @@
+seleccionArgentina = {
+    "10": {"Nombre": "Lionel Messi", "Edad": 35, "Altura": 1.70, "Precio": "18 Millones", "Posicion": "Extremo Derecho"},
+    "19": {"Nombre": "Nicolas Otamendi", "Edad": 34, "Altura": 1.83, "Precio": "1 Millon", "Posicion": "Defensa Central"},
+    "24": {"Nombre": "Nahuel Molina", "Edad": 27, "Altura": 1.75, "Precio": "20 Millones", "Posicion": "Lateral Derecho"},
+    "22": {"Nombre": "Lautaro Martinez", "Edad": 27, "Altura": 1.74, "Precio": "95 Millones", "Posicion": "Delantero"},
+    "3": {"Nombre": "Lisandro Martinez", "Edad": 26, "Altura": 1.75, "Precio": "50 Millones", "Posicion": "Defensa Central"},
+    "20": {"Nombre": "Alexis Mac Allister", "Edad": 25, "Altura": 1.76, "Precio": "100 Millones", "Posicion": "Mediocampista"},
+    "9": {"Nombre": "Julian Alvarez", "Edad": 24, "Altura": 1.70, "Precio": "100 Millones", "Posicion": "Delantero"}
+}
+
+for llave, valor in seleccionArgentina.items():
+    print(llave, valor)
+
+print("Tenemos cargados en el diccionario la cantidad de jugadores:", end=" ")
+print(len(seleccionArgentina))
