a856100cd289567c546d81d84f01b568da0c9faf
Samira <bazsamira79@gmail.com>
2025-08-27 11:37:05 -0300

    "Reesubí parte de la clase 2"

--- Python/Clase_2/Diccionario/Diccionario_1.py
+++ Python/Clase_2/Diccionario/Diccionario_1.py
@@ -1,47 +1,51 @@
-#"Maradona": 10 Un diccionario esta compuesto por dos elementos
-# Una Llave y un Valor
+#DICCIONARIOS
+"""
+Definición: Un diccionario es una estructura que guarda datos en pares: cada dato tiene 
+una clave (key) y un valor (value). Es como una agenda: buscás por nombre (clave) 
+y encontrás el número (valor). 
+"""
+
+#"Maradona":10 #Maradona es la llave y el 10 es el tipo de dato que va a almacenar la llave, ambas partes forman al diccionario
 #dict(key,value)
 diccionario = {
-    "IDE": "Integrated Development Environment",
-    "POO": "Programacíon Orientada a Objetos",
-    "SABD": "Sistema de Administracion de Base de Datos"
+    "IDE":"Integrated Development Environment", #Para finalizar un elemento en el diccionario hay que poner coma
+    "POO":"Programación Orientada a Objetos",
+    "SABD":"Sistema de Administración de Base de Datos "
 }
-
-#Verificar la cantidad de elementos del diccionario
-print(len(diccionario))
+print(len(diccionario)) #La función len nos muestra la cantidad de elementos que hay en el diccionario
 print(diccionario)
 
-#Acceder a un diccionario con la llave(key)
+#Acceder a un diccionario con la llave
 print(diccionario["IDE"])
 
-#Otra forma de recuperar un elemento
+#Otra forma de acceder a un elemento
 print(diccionario.get("POO"))
 print(diccionario.get("SABD"))
 
 #Modificamos elementos
-diccionario["IDE"] = "Entorno de Desarrollo Integrado"
+diccionario["IDE"] = "Entorno de desarrollo integrado"
 print(diccionario)
 
-#Como recorrer los elementos
-for termino in diccionario:
+#Cómo recorrer/mostrar los elementos
+for termino in diccionario: #Recorre mostrando solo las llaves
     print(termino)
 
-#Necesitamos una funcion para recorrer un diccionario
-for termino, valor in diccionario.item():
+#Necesitamos la función items para recorrer un diccionario
+for termino, valor in diccionario.items():
     print(termino, valor)
 
 #Otras maneras de acceder a un diccionario
-for termino in diccionario.keys():
-    print(termino) #Muestra una sola llave
+for termino in diccionario.keys(): #Muestra solo las llaves
+    print(termino)
 
-for valor in diccionario.values(): #Usamos una funcion para acceder a un valor
+for valor in diccionario.values(): #Muestra solo el valor
     print(valor)
 
-#Comprobar la existencia de algun elemento
-print("IDE" in diccionario) #Devuelve un boolean
+#Comprobar la existencia de algún elemento
+print("IDE" in diccionario) #Devuelve un booleano
 
 #Agregar un elemento
-diccionario["PK"] = "Primer key"
+diccionario["PK"] = "Primary Key"
 print(diccionario)
 
 #No es posible agregar llaves duplicadas al diccionario, si se agrega una llave existente sobreescribe
@@ -51,5 +55,9 @@ print(diccionario)
 diccionario.pop("SABD")
 print(diccionario)
 
+#Vaciar un diccionario
+diccionario.clear()
+print(diccionario)
+
 #Eliminar diccionario
-del diccionario #el diccionario se borro
+del diccionario #El diccionario se borra
